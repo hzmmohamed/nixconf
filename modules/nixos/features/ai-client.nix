a37df4f9cee9da9727ef3263a68a9aea7022ff87
Hazem Fahmi <hzmmohamed@gmail.com>
2026-03-28 19:31:56 +0200

    Add ai-client

---
+++ modules/nixos/features/ai-client.nix
@@ -0,0 +1,23 @@
+{...}: {
+  flake.nixosModules.ai-client = {pkgs, ...}: {
+    programs.firefox.policies.ManagedBookmarks = [
+      {toplevel_name = "AI";}
+      {
+        name = "LibreChat";
+        url = "http://peacelily:3080";
+      }
+      {
+        name = "llama-swap";
+        url = "http://peacelily:9292";
+      }
+      {
+        name = "Qdrant";
+        url = "http://peacelily:6333/dashboard";
+      }
+    ];
+
+    environment.systemPackages = with pkgs; [
+      openai-whisper-cpp
+    ];
+  };
+}
